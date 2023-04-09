65f1a2cae2c5aeddc5cc72320fdc75165039c7e8
Hieu Bui <hieubnd149@gmail.com>
2023-04-09 16:04:20 +0700

    Init nvim configuration from kikstarter.nvim

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,10 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  -- File Explorer
+  { 'nvim-tree/nvim-tree.lua' },
+  -- Icon for file icons
+  { 'nvim-tree/nvim-web-devicons' },
+}
